27ffb50ebbe7257fab5d2feb77a3c3a70ce9e824
manu.s <manu0501@icloud.com>
2024-06-03 13:15:06 +0200

    Continued front-end development

--- src/pages/Expansions.js
+++ src/pages/Expansions.js
@@ -1,22 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Background from '../components/Background';
 import Footer from '../components/Footer';
 import { Helmet } from 'react-helmet';
+
 const Espansioni_page = () => {
+  const [sets, setSets] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.pokemontcg.io/v2/sets')
+      .then(response => response.json())
+      .then(data => {
+        const filteredSets = data.data.filter(set => new Date(set.releaseDate) <= new Date('2023-11-03'));
+        setSets(filteredSets);
+      });
+  }, []);
+
+  const groupedSets = sets.reduce((acc, set) => {
+    if (!acc[set.series]) {
+      acc[set.series] = [];
+    }
+    acc[set.series].push(set);
+    return acc;
+  }, {});
+  
   return (
     <>
-    <Helmet>
-    <title>PokeDB - Espansioni</title>
-    </Helmet>
-    <>
-    <Background />
-    <Header />
-    <section className="homepage">
-      
-    </section>
-    <Footer />
-    </>
+      <Helmet>
+        <title>PokeDB - Espansioni</title>
+      </Helmet>
+      <Header />
+      <div className='container_expansion'>
+        {Object.keys(groupedSets).map(series => (
+          <div key={series} className="series-group">
+            <h2>{series}</h2>
+            <div className="sets-grid">
+              {groupedSets[series].map(set => (
+                <div key={set.id} className="set-card">
+                  <img src={set.images.logo} alt={set.name} />
+                  <div className="set-info">
+                    <h3>{set.name}</h3>
+                    <p>Release Date: {set.releaseDate}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <Footer />
     </>
   );
 };
